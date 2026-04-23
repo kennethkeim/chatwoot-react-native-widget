eae32ea55563b0d50618215fc6c5dce3d1e589d2
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2026-04-23 09:00:14 -0400

    Revert "feat: add loader for widget (#54)"
    This reverts the relevant parts of commit cd280c2642b6d054606df81d55062018f76091ae.

--- Example/src/WebView.js
+++ Example/src/WebView.js
@@ -1,5 +1,5 @@
-import React, { useState, useMemo } from 'react';
-import { StyleSheet, Linking, View, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { StyleSheet, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 import PropTypes from 'prop-types';
 import { isJsonString, storeHelper, generateScripts, getMessage } from './utils';
@@ -30,7 +30,6 @@ const WebViewComponent = ({
   closeModal,
 }) => {
   const [currentUrl, setCurrentUrl] = React.useState(null);
-  const [loading, setLoading] = useState(true);
   let widgetUrl = `${baseUrl}/widget?website_token=${websiteToken}&locale=${locale}`;
 
   if (cwCookie) {
@@ -60,71 +59,43 @@ const WebViewComponent = ({
     setCurrentUrl(newNavState.url);
   };
 
-  const opacity = useMemo(() => {
-    if (loading) {
-      return {
-        opacity: 0,
-      };
-    }
-    return {
-      opacity: 1,
-    };
-  }, [loading]);
-
-  const renderLoadingComponent = () => {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="small" />
-      </View>
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <WebView
-        source={{
-          uri: widgetUrl,
-        }}
-        onMessage={(event) => {
-          const { data } = event.nativeEvent;
-          const message = getMessage(data);
-          if (isJsonString(message)) {
-            const parsedMessage = JSON.parse(message);
-            const { event: eventType, type } = parsedMessage;
-            if (eventType === 'loaded') {
-              const {
-                config: { authToken },
-              } = parsedMessage;
-              storeHelper.storeCookie(authToken);
-            }
-            if (type === 'close-widget') {
-              closeModal();
-            }
+    <WebView
+      source={{
+        uri: widgetUrl,
+      }}
+      onMessage={(event) => {
+        const { data } = event.nativeEvent;
+        const message = getMessage(data);
+        if (isJsonString(message)) {
+          const parsedMessage = JSON.parse(message);
+          const { event: eventType, type } = parsedMessage;
+          if (eventType === 'loaded') {
+            const {
+              config: { authToken },
+            } = parsedMessage;
+            storeHelper.storeCookie(authToken);
           }
-        }}
-        scalesPageToFit
-        useWebKit
-        sharedCookiesEnabled
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        style={[styles.WebViewStyle, opacity]}
-        injectedJavaScript={injectedJavaScript}
-        onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
-        onNavigationStateChange={handleWebViewNavigationStateChange}
-        onLoadStart={() => setLoading(true)}
-        onLoadProgress={() => setLoading(true)}
-        onLoadEnd={() => setLoading(false)}
-        scrollEnabled
-      />
-      {loading && renderLoadingComponent()}
-    </View>
+          if (type === 'close-widget') {
+            closeModal();
+          }
+        }
+      }}
+      scalesPageToFit
+      useWebKit
+      sharedCookiesEnabled
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      style={styles.WebViewStyle}
+      injectedJavaScript={injectedJavaScript}
+      onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+      onNavigationStateChange={handleWebViewNavigationStateChange}
+      scrollEnabled
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   modal: {
     flex: 1,
     borderRadius: 4,
@@ -132,24 +103,6 @@ const styles = StyleSheet.create({
   },
   webViewContainer: {
     flex: 1,
-  },
-  WebViewStyle: {
-    flex: 1,
-  },
-  loadingContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-  },
-  loadingText: {
-    marginTop: 10,
-    fontSize: 16,
-    color: '#666',
   },
 });
 
